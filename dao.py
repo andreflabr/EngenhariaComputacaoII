82e14d385235e3fdafb64cf2bac3c810dba70656
Vinicius da Silva <73319678+ViniciusX-666@users.noreply.github.com>
2022-04-18 20:51:08 -0300

    if

--- dao.py
+++ dao.py
@@ -1,17 +1,25 @@
 from models import Usuario
 
 SQL_CRIA_CLIENTE = 'INSERT into cliente (id,nome,senha,email,cpf) values (%s,%s,%s,%s,%s)'
-SQL_DELETA_CLIENTE = 'DELETE from cliente where idcliente=%s'
+SQL_DELETA_CLIENTE = 'DELETE from cliente where id=%s'
 SQL_ATUALIZA_CLIENTE = 'UPDATE cliente SET nome=%s, senha=%s, email=%s, cpf=%s where id=%s '
-SQL_USUARIO_POR_ID = 'SELECT idcliente,nome,senha,email,cpf from cliente where idcliente=%s '
-SQL_BUSCA_CLIENTE = 'SELECT id,nome,senha,email,cpf from cliente where idcliente=%s '
-SQL_BUSCA_POR_ID ='SELECT id,nome,email,cpf from cliente where id=%s'
+SQL_USUARIO_POR_ID = 'SELECT id,nome,senha,email,cpf from cliente where id=%s '
+SQL_BUSCA_CLIENTE = 'SELECT id,nome,senha,email,cpf from cliente where email=%s '
+SQL_BUSCA_POR_ID ='SELECT id,nome,email,cpf from cliente where email=%s'
 
 
 def traduz_usuario(tupla):
-    return Usuario(tupla[0],tupla[3],tupla[4])
+    return Usuario(tupla[0],tupla[3],tupla[2])
 
 class UsuarioDao:
+    #Busca por id
+    def busca_por_id(self,id):
+            cursor = self.__db.connection.cursor()
+            cursor.execute(SQL_USUARIO_POR_ID,(id,))
+            dados = cursor.fetchone()
+            cliente = traduz_usuario(dados) if dados else None
+            return cliente   
+
     def __init__(self,db):
         self.__db=db
         #cria e atualiza usuario
@@ -31,10 +39,5 @@ class UsuarioDao:
         def deletar_usuario(self,id):
             self.__db.connection.cursor().execute(SQL_DELETA_CLIENTE,(id,))
             self.__db.connection.commit()   
-        #Busca por id
-        def SQL_BUSCA_POR_ID(self,id):
-            cursor = self.__db.connection.cursor()
-            cursor.execute(SQL_USUARIO_POR_ID,(id,))
-            dados = cursor.fetchone()
-            cliente = traduz_usuario(dados) if dados else None
-            return cliente     
+        
+          
