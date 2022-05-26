e824319f22325ec3890e8f6ccf5b5d730a49ffcc
Vinicius da Silva <73319678+ViniciusX-666@users.noreply.github.com>
2022-05-25 23:00:46 -0300

    s

--- static/teste.js
+++ static/teste.js
@@ -1,18 +1,16 @@
-$(document).ready(function () {
-  $("#botao").click(function () {
-    alert("Seja bem vindo");
-  });
-});
+// $(document).ready(function () {
+//   $("#botao").click(function () {
+//     alert("Seja bem vindo");
+//   });
+// });
 
 $(document).ready(function () {
   $("#valor").mask("R$ 00,00");
 });
 
 $(document).ready(function () {
-  $("#sair").change(function () {
-    $("#sair").click(function () {
-      $("#sair").attr("href", "http://127.0.0.1:5000/logout");
-      $("#sair").html("Sair");
-    });
+  $("#sair").click(function () {
+    $("#sair").attr("disabled", true);
+    $("#sair").text("Sair");
   });
 });
