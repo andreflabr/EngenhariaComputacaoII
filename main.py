ac55a642fe8793f78c669284a044335b1b0aaf8f
André <87048023+andreflabr@users.noreply.github.com>
2022-06-06 21:48:13 -0300

    IF

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app.secret_key = 'engenharia'
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_PASSWORD'] = '123456'
 app.config['MYSQL_DB'] = 'mf'
 app.config['MYSQL_PORT'] = 3306
 db = MySQL(app)
